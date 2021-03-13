2d996763777c4942aeab54ac58d4051e830b87d0
Emil Ekström <ekstromemil@gmail.com>
2021-03-13 13:56:17 +0200

    Lets get started

---
+++ rm-routeplanner/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Planner from '../views/Planner.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/planner',
+    name: 'Planner',
+    component: Planner
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ rm-routeplanner/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>Hello, this is the home page!</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
