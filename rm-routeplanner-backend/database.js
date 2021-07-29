c9f16841b8c4c399e1239409ddf114e942311b90
Emil Ekström <ekstromemil@gmail.com>
2021-07-29 15:55:25 +0300

    Add typecasting to database.js and update mysql => mysql2

--- rm-routeplanner-backend/database.js
+++ rm-routeplanner-backend/database.js
@@ -1,4 +1,4 @@
-let mysql = require('mysql');
+let mysql = require('mysql2');
 let config = require("./config.js");
 
 let pool = mysql.createPool({
@@ -6,7 +6,27 @@ let pool = mysql.createPool({
     user:               config.db_username,
     password:           config.db_password,
     database:           config.db,
-    connectionLimit:    10
+    connectionLimit:    10,
+    typeCast: function castField( field, useDefaultTypeCasting ) {
+
+		// If the field if a JSON field (BLOB)
+		if ( ( field.type === "BLOB" ) && ( field.length == 4294967295 ) ) {
+
+			let value = field.string();
+
+            // Try converting the BLOB to JSON
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                // If the parsing fails (Not JSON), then just return the original value
+                return value;
+            }
+
+		}
+
+		return( useDefaultTypeCasting() );
+
+	}
 });
 
 module.exports = pool;
