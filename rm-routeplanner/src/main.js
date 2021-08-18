48da218302554ddfd9c1b87362bc40514ae4cea7
Emil Ekström <ekstromemil@gmail.com>
2021-08-18 22:20:21 +0300

    Add element UI

--- rm-routeplanner/src/main.js
+++ rm-routeplanner/src/main.js
@@ -6,11 +6,16 @@ import 'bootstrap';
 //Toasts
 import VueToasts from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css';
+//ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(VueToasts, {
     position: 'bottom',
     duration: 5000
 });
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 Vue.prototype.$apiURL = 'http://localhost:3000';
