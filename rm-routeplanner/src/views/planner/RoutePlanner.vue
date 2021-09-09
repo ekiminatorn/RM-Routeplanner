5e1109bfa0648fd210b638a9b06b385740519798
Emil Ekström <ekstromemil@gmail.com>
2021-09-09 11:35:05 +0300

    Mellanpush

--- rm-routeplanner/src/views/planner/RoutePlanner.vue
+++ rm-routeplanner/src/views/planner/RoutePlanner.vue
@@ -1,44 +1,43 @@
 <template>
   <div class="routeplanner" id="routeplanner">
+    <PlannerMap> </PlannerMap>
 
-      <PlannerMap>
-      </PlannerMap>
 
-            <div v-drag="{handle: '#card-header'}" class="rm-card card">
-          <div class="card-body">
-
-              <div class="rm-card-header" id="card-header">
-                <h5 class="card-title rm-card-title"> Reitti 1</h5>
-                <h5 class="rm-card-options bi bi-three-dots-vertical"></h5>
-              </div>
-                <el-collapse :accordion="accordionMode">
-                    <el-collapse-item title="Waypoint 1" name ="1">
-                        Waypoint content
-                    </el-collapse-item>
-                    <el-collapse-item title="Waypoint 2" name ="2">
-                        Waypoint content
-                    </el-collapse-item>
-                                        <el-collapse-item title="Waypoint 1" name ="3">
-                        Waypoint content
-                    </el-collapse-item>
-                    <el-collapse-item title="Waypoint 2" name ="4">
-                        Waypoint content
-                    </el-collapse-item>
-                                        <el-collapse-item title="Waypoint 1" name ="5">
-                        Waypoint content
-                    </el-collapse-item>
-                    <el-collapse-item title="Waypoint 2" name ="6">
-                        Waypoint content
-                    </el-collapse-item>
-                                        <el-collapse-item title="Waypoint 1" name ="7">
-                        Waypoint content
-                    </el-collapse-item>
-                </el-collapse>
-
+    <div v-for="(element, index) in shifts" :key="element.routeId" v-drag="{ handle: '#card-header' }" class="rm-card card">
+      <div class="card-body">
+        <div class="rm-card-header" id="card-header">
+          <h5 class="card-title rm-card-title">Reitti {{ index }}</h5>
+          <h5 class="rm-card-options bi bi-three-dots-vertical"></h5>
+        </div>
+        <el-collapse :accordion="accordionMode">
+          <draggable :list="element.waypoints" group="routes" handle=".handle">
+          <el-collapse-item v-for="(waypoint, index) in element.waypoints" :key="waypoint.id" :name="index">
+            <template slot="title" class="user-select-none">
+              <i class="bi bi-arrows-move handle"></i>
+              {{ waypoint.name }}
+              <span class="ms-1 badge rounded-pill bg-primary">HS</span>
+              <span class="ms-1 badge rounded-pill bg-warning">Lehdet</span>
+              <span class="ms-1 badge rounded-pill bg-info"> 19:00</span>
+            </template>
+            <div class="d-flex flex-row">
+            <select class="">
+              <option selected>Projekti</option>
+              <option value="1">HS</option>
+              <option value="2">Fortum</option>
+            </select>
+            <el-radio-group v-model="radio4" size="mini">
+              <el-radio-button label="Rakennus"></el-radio-button>
+              <el-radio-button label="Lehdet"></el-radio-button>
+              <el-radio-button label="Purku"></el-radio-button>
+            </el-radio-group>
             </div>
-       </div>
+          </el-collapse-item>
+          </draggable>
+        </el-collapse>
+      </div>
+    </div>
 
-<!--
+    
 
     <div class="col-3" v-for="(element) in shifts" :key="element.routeId">
       <h3>Draggable 1</h3>
@@ -53,33 +52,33 @@
       </draggable>
     </div>
 
--->
+
   </div>
 </template>
 
 <script>
-//import draggable from "vuedraggable";
+import draggable from "vuedraggable";
 import PlannerMap from "@/components/planner/PlannerMap.vue";
-
 
 export default {
   name: "RoutePlanner",
   components: {
-    PlannerMap
+    PlannerMap, draggable
   },
   data: function () {
     return {
       loading: true,
       mode: null,
       accordionMode: false,
+      radio4: "Lehdet",
       shifts: [
         {
           routeId: "11",
-          waypoints: [{ id: "Emil" }, { id: "Johnny" }],
+          waypoints: [{ id: "1", name:"Tripla" }, { id: "2", name:"Citycenter" }],
         },
         {
           routeId: "12",
-          waypoints: [{ id: "Sara" }, { id: "Peter" }],
+          waypoints: [{ id: "1", name:"Redi" }, { id: "2", name:"Hertsi" }],
         },
       ],
     };
@@ -107,35 +106,32 @@ export default {
 
 
 <style lang="scss" scoped>
-
 .rm-card {
-    width: 20vw;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    border-radius: 5px;
+  width: 20vw;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 5px;
 
-    position: absolute;
-    z-index: 1000;
-    top: 110px;
-    right: 150px;
-    max-height: 90vh;
-    overflow: scroll;
-    overflow-x: hidden;
+  position: absolute;
+  z-index: 1000;
+  top: 110px;
+  right: 150px;
+  max-height: 90vh;
+  overflow: scroll;
+  overflow-x: hidden;
 }
 
 .rm-card-header {
-
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 
 .rm-card-options {
-    float: right;
+  float: right;
 }
 
 .rm-card-title {
-    cursor: move;
+  cursor: move;
 }
 
 .routeplanner {
